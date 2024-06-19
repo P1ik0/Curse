b93a3700fb1607bdbcd8959869f401685d390424
Stella Demchenko <144791455+P1ik0@users.noreply.github.com>
2024-06-19 12:40:48 +0300

    Homework lesson12

--- src/main/java/Homework/Lesson12/Student.java
+++ src/main/java/Homework/Lesson12/Student.java
@@ -1,24 +1,70 @@
 package Homework.Lesson12;
+
 import Lesson11.Student.*;
+
 public class Student {
 }
-class StudentTest{
 
-   public  void ravno1(Student student, Student student2)
-   {
-       if (student.equals(student2))
-       {
-           System.out.println("равны");
-       }
-       else
-       {
-           System.out.println("не равны");
-       }
-   }
+class StudentTest {
 
-    public static void main(String[] args) {
-
-        Lesson11.Student student = new Lesson11.Student("Anna", 12,6.3);
-        Lesson11.Student student2 = new Lesson11.Student("Oleg",1,3.2);
+    public void ravno1(Lesson11.Student student, Lesson11.Student student2) {
+        if (student.name.equals(student2.name) && student.course == student2.course && student.grade == student2.grade) {
+            System.out.println("равны");
+        } else {
+            System.out.println("не равны");
+        }
     }
+
+    public void ravno2(Lesson11.Student student, Lesson11.Student student2)
+    {
+        if (student.name.equals(student2.name))
+        {
+            if (student.course == student2.course   )
+            {
+                if (student.grade == student2.grade)
+                {
+                    System.out.println("Одинаковые оценки. имена. и курсы");
+                }
+                else
+                {
+                    System.out.println("Одинаковые курсы. имена");
+                }
+            } else
+            {
+                System.out.println("Одинаковіе имена");
+            }
+
+        }else if (student.course != student2.course && student.grade != student2.grade){
+            System.out.println("имена разные. оценки разные. курсы разные");
+        }
+        else if (student.course == student2.course && student.grade == student2.grade)
+        {
+            System.out.println("имена разные. оценки одинаковые. курсы одинаковые");
+        }
+        else if (student.course == student2.course && student.grade != student2.grade)
+        {
+            System.out.println("имена разные. оценки разные. курсы одинаковые");
+        }
+        else
+        {
+            System.out.println("имена разные. оценки одинаковые. курсы разные");
+        }
+
+
+    }
+
+
+
+
+
+public static void main(String[] args) {
+
+    Lesson11.Student student = new Lesson11.Student("Anna", 1, 3.2);
+    Lesson11.Student student2 = new Lesson11.Student("Anna", 1, 3.2);
+
+    StudentTest studentTest = new StudentTest();
+    studentTest.ravno1(student, student2);
+    studentTest.ravno2(student, student2);
+
+}
 }
