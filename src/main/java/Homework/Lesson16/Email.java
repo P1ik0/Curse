89f3d96332fac61c87acdc4295be6ffcab5cbfc4
Stella Demchenko <144791455+P1ik0@users.noreply.github.com>
2024-06-20 19:32:26 +0300

    Lesson  Homework 16

--- src/main/java/Homework/Lesson16/Email.java
+++ src/main/java/Homework/Lesson16/Email.java
@@ -1,33 +1,22 @@
 package Homework.Lesson16;
 
+import Homework.Lesson10.p4.p5.E;
+
 public class Email {
-    String email;
-
-    Email(String email){
-        this.email = email;
-    }
-    public static void emailgek(String em1)
-    {
-
-        String s1 = em1;
-        boolean b3 = s1.endsWith(";");
-        if (b3 == true)
-        {
-            int i1 = s1.indexOf('@');
-            int i2 = s1.indexOf('.',i1);
-            String c1 = s1.substring(i1+1,i2);
-            System.out.println(c1);
+    void email(String email){
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while (c < email.length() - 1){
+            a = email.indexOf('@', c);
+            b = email.indexOf('.', c);
+            c = email.indexOf(';', c + 1);
+            System.out.println(email.substring(a+1, b));
         }
-        else
-        {
-            System.out.println("В конце почты должно ';' ");
-        }
-
     }
+
     public static void main(String[] args) {
-
-        Email em1 = new Email("asfs@mail.ru;");
-        emailgek(em1.email);
-
+        Email l16 = new Email();
+        l16.email("asd@gmail.com; wdsa@hotmail.fr; fasdwrsa@hobit.gore;");
     }
 }
