975358ddea3a4f577b1495ee677947bfb3da177a
Stella Demchenko <144791455+P1ik0@users.noreply.github.com>
2024-06-27 12:57:21 +0300

    Homework 17

--- src/main/java/Homework/Lesson17/Ravno.java
+++ src/main/java/Homework/Lesson17/Ravno.java
@@ -1,19 +1,31 @@
 package Homework.Lesson17;
 
-public class Ravno {
 
-    public static void ravenstvo(StringBuilder s1, StringBuilder s2)
-    {
-        StringBuilder s3 = s1;
-        StringBuilder s4 = s2;
-        System.out.println( "s  и  б одинаковые ?: " + s3.equals(s4));
+    public class Ravno {
 
+        public static void ravenstvo(StringBuilder s1, StringBuilder s2) {
+            int length1 = s1.length();
+            int length2 = s2.length();
+
+            if (length1 != length2) {
+                System.out.println(false);
+                return;
+            }
+
+            for (int i = 0; i < length1; i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    System.out.println(false);
+                    return;
+                }
+            }
+
+            System.out.println(true);
+        }
+
+        public static void main(String[] args) {
+            StringBuilder s = new StringBuilder("Ananas");
+            StringBuilder b = new StringBuilder("Ananas");
+            Ravno.ravenstvo(s, b);
+        }
     }
-    public static void main(String[] args) {
-        StringBuilder s = new StringBuilder("Ananas");
-        StringBuilder b = new StringBuilder("Ananas");
-        Ravno.ravenstvo(s,b);
 
-
-    }
-}
