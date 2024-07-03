1a49cd5bb80c00c8f309f51e7878b92f77d559eb
Stella Demchenko <144791455+P1ik0@users.noreply.github.com>
2024-07-03 21:07:59 +0300

    Lesson25

--- src/main/java/Lesson25/Test4.java
+++ src/main/java/Lesson25/Test4.java
@@ -5,7 +5,7 @@ public class Test4 {
         String [] array1  = {"privet", " poka"};
         Object [] array2 = array1;
         String [] array3 = ((String[])array2);
-       // array3[0] = new StringBuilder("ok");
+        // array3[0] = new StringBuilder("ok");
         array2[0] = new StringBuilder("ok");
 
     }
